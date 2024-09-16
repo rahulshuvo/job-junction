74db311fa50301558c2d65400fe6019d3c5013d5
rahulshuvo <rahulshuvo5@gmail.com>
2024-09-16 22:01:45 +0200

    frontend file structure added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,86 @@
-import './App.css'
-import { Button } from './components/ui/button'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import AppLayout from "./layouts/app-layout";
+//import ProtectedRoute from "./components/protected-route";
+import { ThemeProvider } from "./components/theme-provider";
+import LandingPage from "./pages/landing";
+import Onboarding from "./pages/onboarding";
+import JobListing from "./pages/jobListing";
+import PostJob from "./pages/post-job";
+import MyJobs from "./pages/my-jobs";
+import SavedJobs from "./pages/saved-jobs";
+import JobPage from "./pages/job";
+
+
+
+import "./App.css";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout/>,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage/>,
+      },
+      {
+        path: "/onboarding",
+        element: (
+          //<ProtectedRoute>
+            <Onboarding/>
+          //</ProtectedRoute>
+        ),
+      },
+      {
+        path: "/jobs",
+        element: (
+          //<ProtectedRoute>
+            <JobListing/>
+          //</ProtectedRoute>
+        ),
+      },
+      {
+        path: "/post-job",
+        element: (
+          //<ProtectedRoute>
+            <PostJob/>
+          //</ProtectedRoute>
+        ),
+      },
+      {
+        path: "/my-jobs",
+        element: (
+          //<ProtectedRoute>
+            <MyJobs/>
+          //</ProtectedRoute>
+        ),
+      },
+      {
+        path: "/saved-jobs",
+        element: (
+          //<ProtectedRoute>
+            <SavedJobs/>
+          //</ProtectedRoute>
+        ),
+      },
+      {
+        path: "/job/:id",
+        element: (
+          //<ProtectedRoute>
+            <JobPage/>
+          //</ProtectedRoute>
+        ),
+      },
+    ],
+  },
+]);
 
 function App() {
-
   return (
-      <div>
-        <Button variant="destructive">Hello</Button>
-      </div>
-  )
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
